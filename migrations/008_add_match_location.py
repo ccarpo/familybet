05ae0ff31f04fa86edd3209fc480c5296a33b62d
CCarpo <ccarpo@gmx.net>
2026-06-17 16:30:00 +0200

    fixes

--- migrations/008_add_match_location.py
+++ migrations/008_add_match_location.py
@@ -4,21 +4,20 @@ Revision ID: 008
 Revises: 007
 Create Date: 2025-06-17 14:25:00
 """
-from alembic import op
-import sqlalchemy as sa
 
-# revision identifiers, used by Alembic
 revision = '008'
 down_revision = '007'
 branch_labels = None
 depends_on = None
 
 
-def upgrade():
-    # Add location column to matches table
-    op.add_column('matches', sa.Column('location', sa.String(200), nullable=True))
+def upgrade(conn):
+    """Add location column to matches table using raw SQL for SQLite compatibility."""
+    conn.execute('ALTER TABLE matches ADD COLUMN location VARCHAR(200)')
 
 
-def downgrade():
-    # Remove location column
-    op.drop_column('matches', 'location')
+def downgrade(conn):
+    """Remove location column - SQLite doesn't support DROP COLUMN directly."""
+    # SQLite doesn't support dropping columns, requires table recreation
+    # For downgrade, you'd need to create new table without location, copy data, drop old, rename
+    pass

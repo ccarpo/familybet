ddff7204f4f2a9f4a809f026f7e2587b50efe74d
CCarpo <ccarpo@gmx.net>
2026-06-17 16:31:05 +0200

    fix

--- migrations/008_add_match_location.py
+++ migrations/008_add_match_location.py
@@ -11,13 +11,6 @@ branch_labels = None
 depends_on = None
 
 
-def upgrade(conn):
+def migrate(conn):
     """Add location column to matches table using raw SQL for SQLite compatibility."""
     conn.execute('ALTER TABLE matches ADD COLUMN location VARCHAR(200)')
-
-
-def downgrade(conn):
-    """Remove location column - SQLite doesn't support DROP COLUMN directly."""
-    # SQLite doesn't support dropping columns, requires table recreation
-    # For downgrade, you'd need to create new table without location, copy data, drop old, rename
-    pass
